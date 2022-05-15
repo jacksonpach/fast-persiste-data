6e3504483c9f89ffe09e58ee4538dd0e7f997eba
Jackson Pacheco <jackson.pacheco@terceiros.uniasselvi.com.br>
2022-05-14 22:12:58 -0300

    feat(API): Create first pos method.

---
+++ api.py
@@ -0,0 +1,29 @@
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+
+class Person(BaseModel):
+    name: str
+    phone: str
+    email: str
+    postal_zip: Union[str, None] = None
+    region: str
+    country: str
+    text: str
+    number_range: float
+    currency: str
+    alphanumeric: str = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/persons/")
+async def create_item(person: Person):
+    return person
